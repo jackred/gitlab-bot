46a09bc5d05da2cf7c712c09ee13d25672d52232
jackred <jackred@tutamail.com>
2019-02-09 12:26:30 +0100

    UPDATE start debug

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ A basic discord bot, written in node.js, to send message when there is a push ev
 Creating the image:  
 `sudo docker build -t gitlab-bot .`
 
-Creating the network:  
-`sudo docker network create gitlab-bot-network`
-
 Running the bot:  
 `./start.sh`
 
--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
-home=`pwd`
+here=`pwd`
 
-sudo docker run --network gitlab-bot-network -v $home/config.json:/usr/src/gitlab-bot-build/config.json --log-opt max-size=10m --restart always -dit --name gitlab-bot gitlab-bot
+if [ $# -ne 0 ];
+then
+    if [ $1 = "debug" ];
+    then
+    sudo docker run --rm -v $here/config.json:/usr/src/gitlab-bot-build/config.json --log-opt max-size=10m -it --name gitlab-bot gitlab-bot
+    else
+	echo "wrong argument"
+    fi
+else
+    sudo docker run -v $here/config.json:/usr/src/gitlab-bot-build/config.json --log-opt max-size=10m --restart always -dit --name gitlab-bot gitlab-bot
+fi
+ 
+
