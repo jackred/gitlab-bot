2bb6a71d88a2cfad589913c3ac3b92bd1d1ab03c
jackred <jackred@tutamail.com>
2019-02-10 12:23:37 +0100

    UPDATE gitlab.json is now volume for docker
    you can change the gitlab conf without rebuilding the bot

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 here=`pwd`
+src="/usr/src/gitlab-bot-build/"
 
 if [ $# -ne 0 ];
 then
     if [ $1 = "debug" ];
     then
-    sudo docker run --rm -v $here/config.json:/usr/src/gitlab-bot-build/config.json --log-opt max-size=10m -it --name gitlab-bot gitlab-bot
+    sudo docker run --rm -v $here/config.json:$src/config.json -v $here/gitlab.json:$src/gitlab.json --log-opt max-size=10m -it --name gitlab-bot gitlab-bot
     else
 	echo "wrong argument"
     fi
 else
-    sudo docker run -v $here/config.json:/usr/src/gitlab-bot-build/config.json --log-opt max-size=10m --restart always -dit --name gitlab-bot gitlab-bot
+    sudo docker run -v $here/config.json:$src/config.json -v $here/gitlab.json:$src/gitlab.json --log-opt max-size=10m --restart always -dit --name gitlab-bot gitlab-bot
 fi
  
 
